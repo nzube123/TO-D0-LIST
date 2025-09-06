3d92517849a6bc288de57e7e3f8635dac859f502
Confidence Ndubuisi <samo188jiren@GOKU.com>
2025-09-06 19:41:52 +0100

    started with the store function

--- src/main.js
+++ src/main.js
@@ -1,23 +1,27 @@
 import './style.css';
 // assigning reef to the render function
-let { store, component } = reef;
 let { render } = reef;
+// let { store, component } = reef;
+
 // todo array
-let todo = [];
+// let todo = [];
+let datatodo = { todo: [] };
+
 // get todos function
 function gettodo() {
-  if (!todo.length) {
-    alert("Todo list is empty")
+  if (!datatodo.todo.length) {
+    alert("Todo list is empty");
   } else {
     return `
     <ul>
-      ${todo.map(function (input, index) {
-      return `<li>${input} &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ${Date()}  <button id="clist" data-delete = "${index}">Delete</button> `
-    }).join('')}
+      ${datatodo.todo.map(function (input, index) {
+      return `<li>${input} &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ${Date()}  <button id="clist" data-delete = "${index}">Done</button> `
+    }).join('')};
     </ul>
     `
   }
 }
+
 // various elements like add button inputarea and the section where the data is to be displayed
 let submit = document.getElementById("submit");
 let input = document.getElementById("inputArea");
@@ -26,38 +30,35 @@ let divtodo = document.getElementById("addl");
 //function to be carried out anytime the Add button is clicked
 submit.addEventListener("click", () => {
   if (!input.value) {
-    alert("Input area is empty")
+    alert("Input area is empty");
   } else {
-    todo.push(input.value);
+    datatodo.todo.push(input.value);
     render(divtodo, gettodo());
     input.value = '';
   }
-})
+  if (typeof (Storage) != "undefined") {
+    localStorage.setItem("todolist", JSON.stringify(datatodo.todo));
+  }
 
-// adding value to the localstorage whenever a render function is carried out
-document.addEventListener("reef:render", function () {
-  localStorage.setItem("todolist", JSON.stringify(todo));
-  console.log("was updated");
 })
 
 // setting a default value for the todo array
-todo = JSON.parse(localStorage.getItem("todolist"));
-
+datatodo.todo = JSON.parse(localStorage.getItem("todolist"));
 // initiating the gettodo() function by default
-render(divtodo, gettodo())
+render(divtodo, gettodo());
 
 function removetodo(event) {
   let index = event.target.getAttribute('data-delete');
   if (!index) {
     return;
   } else {
-    todo.splice(index, 1);
-    render(divtodo, gettodo())
+    datatodo.todo.splice(index, 1);
+    render(divtodo, gettodo());
   }
 
   // localstorage (creating a localstorage when ever the submit button is clicked)
-  if (typeof (Storage) != 'undefined') {
-    localStorage.setItem("todolist", JSON.stringify(todo));
+  if (typeof (Storage) !== 'undefined') {
+    localStorage.setItem("todolist", JSON.stringify(datatodo.todo));
   }
 }
 divtodo.addEventListener("click", removetodo);
