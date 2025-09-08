fc723d59861f37c005a6405609d9f852a7097dc3
Confidence Ndubuisi <samo188jiren@GOKU.com>
2025-09-08 16:39:33 +0100

    finally finished with reef

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import './style.css';
 // assigning reef to the render function
-let { render } = reef;
-// let { store, component } = reef;
+// let { render } = reef;
+let { store, component } = reef;
+
+let divtodo = document.getElementById("addl");
+
 
 // todo array
 // let todo = [];
-let datatodo = { todo: [] };
+let datatodo = store({ todo: JSON.parse(localStorage.getItem("todolist")) || [] });
 
 // get todos function
-function gettodo() {
+component(divtodo, function gettodo() {
   if (!datatodo.todo.length) {
     alert("Todo list is empty");
   } else {
@@ -16,16 +19,15 @@ function gettodo() {
     <ul>
       ${datatodo.todo.map(function (input, index) {
       return `<li>${input} &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ${Date()}  <button id="clist" data-delete = "${index}">Done</button> `
-    }).join('')};
+    }).join('')}
     </ul>
     `
   }
-}
+});
 
 // various elements like add button inputarea and the section where the data is to be displayed
 let submit = document.getElementById("submit");
 let input = document.getElementById("inputArea");
-let divtodo = document.getElementById("addl");
 
 //function to be carried out anytime the Add button is clicked
 submit.addEventListener("click", () => {
@@ -33,19 +35,16 @@ submit.addEventListener("click", () => {
     alert("Input area is empty");
   } else {
     datatodo.todo.push(input.value);
-    render(divtodo, gettodo());
     input.value = '';
   }
   if (typeof (Storage) != "undefined") {
     localStorage.setItem("todolist", JSON.stringify(datatodo.todo));
   }
-
 })
 
 // setting a default value for the todo array
 datatodo.todo = JSON.parse(localStorage.getItem("todolist"));
 // initiating the gettodo() function by default
-render(divtodo, gettodo());
 
 function removetodo(event) {
   let index = event.target.getAttribute('data-delete');
@@ -53,12 +52,9 @@ function removetodo(event) {
     return;
   } else {
     datatodo.todo.splice(index, 1);
-    render(divtodo, gettodo());
   }
 
   // localstorage (creating a localstorage when ever the submit button is clicked)
-  if (typeof (Storage) !== 'undefined') {
-    localStorage.setItem("todolist", JSON.stringify(datatodo.todo));
-  }
+  localStorage.setItem("todolist", JSON.stringify(datatodo.todo));
 }
 divtodo.addEventListener("click", removetodo);
